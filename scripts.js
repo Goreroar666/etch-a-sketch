53774c6ccf5ff3159fffb3e4e07122c420942b46
Kasper Mennink <kasper.mennink@hotmail.com>
2021-08-16 23:07:24 +0200

    Made the grid better.

--- scripts.js
+++ scripts.js
@@ -1,27 +1,15 @@
 const sketchPad = document.getElementById("main");
 
-function makeGrid(grid) {
-const container = document.createElement('div');
-container.id = "main";
-container.className = "container";
+function makeGrid(rows, columns) {
+sketchPad.style.setProperty('--grid-rows', rows);
+sketchPad.style.setProperty('--grid-columns', columns);
+for (let i = 0; i < (rows * columns); i++) {
+    let cell = document.createElement('div');
+    sketchPad.appendChild(cell).className = "grid-cell";
+};
+};
+makeGrid(16, 16);
 
-for (let i=0; i<16; i++) {
-    let row = document.createElement('div');
-    row.className = "row";
-    row.id = "row" + i;
-   
-    for (let j=0; j<16; j++) {
-        let box = document.createElement('div');
-        box.className= "box";
-        box.id = "box" + j;
-        row.appendChild(box);
-    }
-    container.appendChild(row);
-}
-grid.appendChild(container);
-}
-makeGrid(document.body);
-
-main.addEventListener ("mouseover", function (event) {
-    event.target.style.background = "red";
+sketchPad.addEventListener ("mouseover", function (event) {
+    event.target.style.background = "green";
 })
